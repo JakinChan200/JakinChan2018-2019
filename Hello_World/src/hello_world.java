411c180ae2d4affaa72ca4ad70cac102fc6f9481
APCS3rd <APCS3rd@MHS-Dreyer-03>
2018-08-21 09:56:02 -0700

    Hello World

---
+++ Hello_World/src/hello_world.java
@@ -0,0 +1,15 @@
+
+/*
+ * Prints the words Hello World
+ * 
+ * author@ Jakin CHan
+ * 
+ */
+public class hello_world {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
