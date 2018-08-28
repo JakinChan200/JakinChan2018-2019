1fd34280cb7bf389543a55312580ebd393bcf6e8
APCS3rd <APCS3rd@MHS-Dreyer-03>
2018-08-28 11:10:15 -0700

    Added date and personalized the message.

--- Hello_World/src/hello_world.java
+++ Hello_World/src/hello_world.java
@@ -1,15 +1,15 @@
 
 /*
- * Prints the words Hello World
+ * Prints the words Hello World and my name
  * 
- * author@ Jakin CHan
- * 
+ * author@ Jakin Chan
+ * 28 August 2018
  */
 public class hello_world {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-
+		System.out.println("-Jakin Chan");
 	}
 
 }
