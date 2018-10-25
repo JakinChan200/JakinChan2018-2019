dc8010d4cadcadc6b1057f4457f3b57acff555e7
APCS3rd <APCS3rd@MHS-Dreyer-03>
2018-10-25 10:54:22 -0700

    Started and worked on Hourglass

--- HourGlassActivity/src/HourGlass.java
+++ HourGlassActivity/src/HourGlass.java
@@ -2,15 +2,27 @@
 public class HourGlass {
 
 	public static void main(String[] args) {
-		System.out.println("|" + printChars("*", 10) + "|");
-		for(int j = 4; j > 0; j--) {
-			printChars(" ", 5-j);
-			System.out.print("\\");
-			printChars("::", j);
-			System.out.println("/");
-		} 
+		String topAndBottom = ("|" + printChars("*", 10) + "|\n");
+		/*String top = topCurve();
+		String bottom = bottomCurve();*/
+		System.out.println(topAndBottom);
 	}
 	
+	public static String topCurve() {
+		String curve = "";
+		for(int j = 4; j > 0; j--) {
+			curve += printChars(" ", 5-j) + "\\" + printChars("::", j) + "/\n";
+		} 
+		return curve;
+	}
+	
+	public static String bottomCurve() {
+		String curve = "";
+		for(int j = 0; j < 4; j--) {
+			curve += printChars(" ", 5-j) + "/" + printChars("::", j) + "\\\n";
+		} 
+		return curve;
+	}
 	public static String printChars(String character, int numPrints) {
 		String line = "";
 		for(int i = 0; i < numPrints; i++) {
